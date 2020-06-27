35e77dd98f0c4bf8fa27c6883be861352bcac2a3
zhuoyin94 <zhuoyin94@163.com>
2020-06-28 01:37:18 +0800

    (refactor)完善了experiment_dtw.py的baseline的检索

--- experiment_dtw.py
+++ experiment_dtw.py
@@ -11,14 +11,16 @@ time series from the raw dataset.
 """
 
 import os
+from time import time
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from tslearn.metrics import dtw
 from utils import LoadSave
 
-from tslearn.metrics import dtw, lb_keogh
 sns.set(style="ticks", font_scale=1.2, palette='deep', color_codes=True)
 np.random.seed(2019)
+
 
 def load_data(path_name=None):
     """Loading *.pkl from path_name, path_name is like: .//data//mnist.pkl"""
@@ -42,16 +44,34 @@ def get_z_normalized_ts(ts=None):
         return (ts - mean_val) / std_val
 
 
-def search_n_similar():
-    pass
+def search_top_n_similar_ts(ts_query=None, data=None, n=10):
+    """For the query ts, search the top-n similar ts in data object, return
+       the searching result.
+    """
+    top_n_searching_res, time_spend = [], []
+    for ind, ts in enumerate(data):
+        start = time()
+        dtw_dist = dtw(ts_query, ts)
+        end = time()
 
+        top_n_searching_res.append([ind, dtw_dist, end-start])
+        time_spend.append(end-start)
 
-def lb_kim():
-    pass
+    # Sorted the results
+    top_n_searching_res = sorted(top_n_searching_res, key=lambda t: t[1])
+    top_n_searching_res = top_n_searching_res[:n]
+
+    searching_res = {}
+    searching_res["top_n_searching_res"] = top_n_searching_res
+    searching_res["mean_time_per_ts"] = np.mean(time_spend)
+    searching_res["std_time_per_ts"] = np.std(time_spend)
+    searching_res["total_searched_ts"] = len(data)
+    searching_res["total_time_spend"] = np.sum(time_spend)
+    return searching_res
 
 
 if __name__ == "__main__":
-    N_TESTS = 10
+    N_SEARCH = 50
     SEARCH_TOP_K = 50
     PATH = ".//data//"
     dataset_name = "heartbeat_mit"
@@ -68,15 +88,20 @@ if __name__ == "__main__":
         data = [get_z_normalized_ts(ts) for ts in data]
 
         # STEP 1: Randomly sampled n ts from the raw dataset
-        selected_ts_ind = sample_n_ts(data, n=N_TESTS)
+        selected_ts_ind = sample_n_ts(data, n=N_SEARCH)
 
-        # Step 2: For each selected ts, search SEARCH_TOP_K ts in the raw dataset,
-        #         return the top-k index.
+        # STEP 2: For each selected ts, search SEARCH_TOP_K ts in the raw dataset,
+        #         return the top-k list results.
         search_res = {}
         for ts_ind in selected_ts_ind:
             ts_query = data[ts_ind]
-            search_res_tmp = search_n_similar(ts_query, data, n=SEARCH_TOP_K)
+            search_res_tmp = search_top_n_similar_ts(ts_query, data, n=SEARCH_TOP_K+1)
             search_res[ts_ind] = search_res_tmp
 
-        # Step 3: Save the SEARCH_TOP_K results in experiment_res
-        experiment_res[name] = search_res[ts_ind]
+        # STEP 3: Save the SEARCH_TOP_K results in experiment_res
+        experiment_res[name] = search_res
+
+    file_processor = LoadSave()
+    new_file_name = ".//data_tmp//" + dataset_name + "_searching_res.pkl"
+    file_processor.save_data(path=new_file_name,
+                             data=experiment_res)
